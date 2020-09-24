52dda29ee75feb976dd8fd4da9de92de9ffb12b0
Alain <jtiarilala.olona@gmail.com>
2020-09-24 22:10:43 +0300

    unique

--- serveur/src/main/java/joe/alain/miniprojet/model/Produit.java
+++ serveur/src/main/java/joe/alain/miniprojet/model/Produit.java
@@ -1,11 +1,7 @@
 package joe.alain.miniprojet.model;
 
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.sql.Date;
@@ -18,9 +14,11 @@ public class Produit {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
+    @Column(unique = true)
     @NotBlank(message = "Nom du produit obligatoire")
     private  String nom;
 
+    @Column(unique = true)
     @NotBlank(message = "Code obligatoire")
     private String code;
 
